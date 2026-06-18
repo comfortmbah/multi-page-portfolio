ac0973e1489de674e697e482e433d75b635a7fad
sylvester <sylvestermbah90@gmail.com>
2026-06-18 08:45:05 -0700

    Create vite@latest and install tailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold">multi-page-portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
