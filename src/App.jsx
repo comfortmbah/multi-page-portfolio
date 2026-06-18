dc20f6a724d1f4da82b085520a7b06e85eb9bbc6
sylvester <sylvestermbah90@gmail.com>
2026-06-18 09:03:37 -0700

    Initaialize the hook form and state

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import { useState } from "react";
 import { useForm }  from "react-hook-form"
 
 function App() {
+  const [step, setStep] = useState(1);
+  const {
+    register,
+    trigger,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-3xl font-bold">multi-page-portfolio</h1>
