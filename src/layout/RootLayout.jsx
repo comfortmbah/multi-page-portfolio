781c520baf7669ba6dff4cc46ad87cca90261eeb
sylvester <sylvestermbah90@gmail.com>
2026-07-03 10:09:01 -0700

    Create RootLayout with shared layout structure

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,8 +1,18 @@
-
+import { Outlet } from "react-router-dom"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div className="min-h-screen flex flex-col">
+        <Navbar />
+
+        <main className="grow">
+            <Outlet />
+        </main>
+
+        <Footer />
+    </div>
   )
 }
 
