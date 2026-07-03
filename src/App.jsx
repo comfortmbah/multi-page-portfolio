7f2f72e2203514f225e76152a2e7267f793d7f46
sylvester <sylvestermbah90@gmail.com>
2026-07-03 07:20:11 -0700

    Set up application routing with react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import RootLayout from './layout/RootLayout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Skills from './pages/Skills';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+import NotFound from './pages/NotFound';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='skills' element={<Skills />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='projects' element={<Projects />} />
+      <Route path='*' element={<NotFound />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
-    <div>
-      <h1 className="text-center">HELLO WORLD</h1>
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
