a5ab19e0534f9730dc00bb04899636b4cb8ea595
sylvester <sylvestermbah90@gmail.com>
2026-07-06 10:23:55 -0700

    Build skill page with reusable skillcard component

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,8 +1,24 @@
-
+import SectionTitle from '../components/SectionTitle';
+import SkillCard from '../components/SkillCard';
+import skills from '../data/skills'
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <section className='max-w-7xl mx-auto px-6 py-16'>
+      <SectionTitle
+        title={'My Skills'}
+        subtitle={'Technologies and tools I use.'}
+      />
+
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+        {skills.map((skill) => (
+          <SkillCard
+            key={skill.id}
+            skill={skill}
+          />
+        ))}
+      </div>
+    </section>
   )
 }
 
