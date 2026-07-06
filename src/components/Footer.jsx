3343ccc3740e20828a1c60d5bc1a0b6a6ee620a3
sylvester <sylvestermbah90@gmail.com>
2026-07-06 03:05:20 -0700

    Create reusable footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,21 @@
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="bg-gray-900 mt-10 text-white">
+        <div className="max-w-7xl mx-auto p-6 flex items-center gap-5 justify-center">
+          <h2 className="text-xl font-semibold">
+            Sly@Dev
+          </h2>
+
+          <p className="text-sm text-gray-400">
+            Build modern web applications with react.
+          </p>
+
+          <p className="text-sm">
+            @ {new Date().getFullYear()} Sly all right reserved.
+          </p>
+        </div>
+    </footer>
   )
 }
 
