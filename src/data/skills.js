5daabdeb6ab7151f1166246caf0e232c6a9b73ec
sylvester <sylvestermbah90@gmail.com>
2026-07-06 09:58:17 -0700

    Add skills data

---
+++ src/data/skills.js
@@ -0,0 +1,24 @@
+const skills = [
+    {
+        id: 1,
+        name: 'HTML',
+    },
+    {
+        id: 2,
+        name: 'CSS',
+    },
+    {
+        id: 3,
+        name: 'Javascript',
+    },
+    {
+        id: 4,
+        name: 'React',
+    },
+    {
+        id: 1,
+        name: 'Git & GitHub',
+    }   
+];
+
+export default skills;
