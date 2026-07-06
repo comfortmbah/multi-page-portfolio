029e8e1dc06d44b46c98776979fca0646bedb433
sylvester <sylvestermbah90@gmail.com>
2026-07-06 05:45:32 -0700

    Add sticky header with tailwind CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   const linkClass = ({ isActive }) => `p-1 inline-block transition-all duration-300 hover:scale-125 hover:text-red-600 ${ isActive ? 'text-red-300 border-b-2' : 'text-gray-900'}`;
 
   return (
-    <header className="bg-white shadow-md">
+    <header className="bg-white shadow-md sticky top-0 z-50">
         <nav className="max-w-7xl mx-auto flex items-center justify-between px-2 py-5">
             <h1 className="text-2xl font-bold text-blue-600">
                 Sly dev
