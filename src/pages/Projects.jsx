c9d7b71c6c8658002be75e8d2aaf0a11d205b8fb
sylvester <sylvestermbah90@gmail.com>
2026-07-06 15:24:19 -0700

    Create responsive projects page with tailwind CSS

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,25 @@
-
+import projects from "../data/projects"
+import ProjectCard from '../components/ProjectCard'
+import SectionTitle from '../components/SectionTitle'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section className="max-w-7xl mx-auto px-6 py-16">
+      <SectionTitle
+        title={'My Projects'}
+        subtitle={"Here are some of the projects I've built."}
+      />
+
+      <div className="grid gap-8 md:grid-cols-2">
+        {projects.map((project) => (
+          <ProjectCard 
+            key={project.id}
+            project={project}
+          />
+        ))}
+      </div>
+    </section>
   )
 }
 
-export default Projects
+export default Projects;
