d48492b60893bd202b47cde1b4393fb9281aa5ad
sylvester <sylvestermbah90@gmail.com>
2026-07-06 23:38:40 -0700

    Add input change handler

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,6 +3,16 @@ import Button from "../components/Button"
 import SectionTitle from "../components/SectionTitle"
 
 const Contact = () => {
+  const [formData, setFormData] = useState({ name: "", email: "", message: "", });
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    setFormData((prevData) => ({ ...prevData, [name]: value }))
+  }
+
+  
   return (
     <div>Contact</div>
   )
