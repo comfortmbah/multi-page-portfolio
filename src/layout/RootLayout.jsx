97d03fe0e37f9b6d23fbc9ad2001e3c116406608
sylvester <sylvestermbah90@gmail.com>
2026-07-07 05:12:43 -0700

    Add scroll to top on route change

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,17 +1,20 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
+import ScrollToTop from "../components/ScrollToTop"
 
 const RootLayout = () => {
   return (
     <div className="min-h-screen flex flex-col">
-        <Navbar />
+      <ScrollToTop />
+        
+      <Navbar />
 
-        <main className="grow">
+      <main className="grow">
             <Outlet />
-        </main>
+      </main>
 
-        <Footer />
+      <Footer />
     </div>
   )
 }
